cf7eab01c310f9545ad3872c4e0c10de93bbd929
AALSAUD <37238021+Arifalsaud@users.noreply.github.com>
2018-03-17 02:59:10 +0700

    Create x1.php

---
+++ src/x1.php
@@ -0,0 +1,156 @@
+<?php
+# Command Line
+
+	if($pesan_datang=='gc')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Anggota Greencore ! :
+1. Alan    (PM)
+2. Arif      (PM)
+3. Dewa    (BC)
+4. Fadhil   (BC)
+5. Ikhwal  (AP)
+6. Nauval  (PM)
+7. Rangga (PM)
+8. Salman (AK)
+9. Septian (PM)
+'.date("j F, Y, g:i a").'
+'.date("B u, e, T, Z, c")
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='gc/db')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Anggota Greencore ! :
+1. Alan    (PM)
+2. Arif      (PM)
+3. Dewa    (BC)
+4. Fadhil   (BC)
+5. Ikhwal  (AP)
+6. Nauval  (PM)
+7. Rangga (PM)
+8. Salman (AK)
+9. Septian (PM)
+
+Anggota Baru :
+1.
+2.
+3.
+...
+'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='ikhwal')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Ikhwal, anaknya baek, suka sama si s***, tapi sian bangat gk direspon sama dia, kalem tapi petakilan, kadang sikapnya gadanta, seneng bangat ama buku kambing conge
+'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='arif')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Arif, orangnya biasa, Jombs, suka bangat buku kambing conge
+'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='septian')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => ''.$pesan_datang.', Belum ada deskripsi
+'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='alan')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => ''.$pesan_datang.', Orangnya pendiem, saking diemnya sampe tidur mulu dikelas, banyak ciwainya, suaranya kaya kambing pengen ka***n, Gans
+'
+									)
+							)
+						);
+				
+	}
+	else
+	if($pesan_datang=='xc')
+	{
+						
+		$push = array(
+							'to' => 'Ub7432ba955730827b36f752bb15af71f',
+				'timestamp' => '1512757840',
+								'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Pesan ini antspace.gq'
+									)
+							)
+						);
+						
+		
+		$client->pushMessage($push);
+				
+	}
+
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+
+file_put_contents('./balasan.json',$result);
+
+
+$client->replyMessage($balas);
