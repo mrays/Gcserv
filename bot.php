58cf3bc7295322de50763520e9ce9ebcba60a225
greencoreg <44386357+greencoreg@users.noreply.github.com>
2019-05-05 14:13:09 +0700

    UPDATE

--- bot.php
+++ bot.php
@@ -1,10 +1,10 @@
 <?php
-// GBOT CMEDIA P
+// SALBOT
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
-$channelAccessToken = 'BrzLsmLonFQsW3XCpsgBFhI7GX/VNVQd8EZDq7z24lHtXzJOcxN5bLWm0Y9OuqHU6Z3mIPYzL4VUabMMuNwoUwX9GFnRMVgJXYdu4TD6JMdxpVPKqy010BLCGFjn+c/603SoSc3UoZPVoEiLsAQHeAdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = 'd434182ad2ffa294b079357b16aa1d2c';//sesuaikan
+$channelAccessToken = ''; //sesuaikan 
+$channelSecret = '';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
